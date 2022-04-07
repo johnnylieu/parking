6cdb377a04db070b0188eb35eb94a115eb01f69e
Johnny <JohnnyLieu@yahoo.com>
2022-04-06 18:40:26 -0700

    testing if available_parking works

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 available_parking = ["small", "big", "medium"]
+
+class Cars:
+    def __init__(self, carSize, owner):
+        self.carSize =carSize
+        self.owner = owner
+    
+    def park(self):
+        for i in range(len(available_parking)):
+            print(available_parking)
