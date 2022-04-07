062c13aac6db2971286d46875db6503bf8a6603c
Johnny <JohnnyLieu@yahoo.com>
2022-04-06 20:52:03 -0700

    after much pondering, it is working now.

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ class Cars:
     
     def park(self):
         for i in range(len(available_parking)):
-            # print(available_parking) # works
-            if available_parking[i] == self.carSize:
+            # print(f"available_parking: {available_parking}") # works
+            # print(f"i: {i}")
+            
+            if self.carSize != "small" and self.carSize != "medium" and self.carSize != "big":
+                # print(f"i: {i}")
+                # print(f"available_parking[i]: {available_parking[i]}")
+                # print(f"available: {available_parking[i]}")
+                print(f"There is no room for your {self.carSize} vehicle")
+                return False
+
+            if self.carSize != "small" or self.carSize != "big" or self.carSize != "medium":
                 # print(available_parking[i]) # works
                 available_parking.remove(available_parking[i])
                 # print(available_parking)
                 print(f"{self.owner} just parked their {self.carSize} vehicle")
                 return True
-            
-            if available_parking[i] != self.carSize:
-                print(f"There is no room for your {self.carSize} vehicle")
-                return False
 
         if len(available_parking) == 0:
             print(f"Sorry {self.owner}, the garage is full")
@@ -27,11 +32,16 @@ class Garage:
     def __init__(self, cars):
         self.cars = cars
 
-park1 = Garage(Cars("small", "Robert")) # can park
-park2 = Garage(Cars("big", "Steve"))# can park
-park3 = Garage(Cars("medium", "Brian"))# can park
-park4 = Garage(Cars("medium", "Brian"))# can't park, garage is full
-park1.cars.park()
-park2.cars.park()
-park3.cars.park()
-park4.cars.park()
+
+if __name__=="__main__":
+    park1 = Garage(Cars("small", "Robert")) # can park
+    park2 = Garage(Cars("small", "Steve"))# can park
+    park3 = Garage(Cars("medium", "Brian"))# can park
+    park4 = Garage(Cars("medium", "Scott"))# can't park, garage is full
+    park5 = Garage(Cars("large", "Johnny"))# can't park, garage is full
+
+    park5.cars.park()
+    park1.cars.park()
+    park2.cars.park()
+    park3.cars.park()
+    park4.cars.park()
