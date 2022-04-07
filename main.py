136f4bb4e8222543a2103693df6af21e3ff37f34
Johnny <JohnnyLieu@yahoo.com>
2022-04-06 18:55:12 -0700

    done, best I could do for the alotted time

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ class Cars:
                 # print(available_parking)
                 print(f"{self.owner} just parked their {self.carSize} vehicle")
                 return True
-            
-            if self.carSize != available_parking[i]:
-                print(f"sorry there is no available parking for your {self.carSize} vehicle")
-                return False
+
+        if len(available_parking) == 0:
+            print(f"Sorry {self.owner}, the garage is full")
+            return False
 
 class Garage:
     def __init__(self, cars):
         self.cars = cars
 
-park1 = Garage(Cars("small", "Johnny"))
-park2 = Garage(Cars("big", "Steve"))
-park3 = Garage(Cars("medium", "Brian"))
-park4 = Garage(Cars("small", "Robert"))
+park1 = Garage(Cars("small", "Robert")) # can park
+park2 = Garage(Cars("big", "Steve"))# can park
+park3 = Garage(Cars("medium", "Brian"))# can park
+park4 = Garage(Cars("medium", "Brian"))# can't park, garage is full
 park1.cars.park()
 park2.cars.park()
 park3.cars.park()
